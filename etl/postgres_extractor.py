be8a312342c4617d3830710ccba8b62fb2966f85
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2022-12-30 21:53:33 +0300

    added context manager for postgres cursor

--- etl/postgres_extractor.py
+++ etl/postgres_extractor.py
@@ -31,53 +31,53 @@ class PostgresExtractor:
         if last_modified is None:
             self.redis_storage.set_key('last_modified', datetime.min)
 
-        cursor = self.connection.cursor()
-        cursor.execute(f"""
-                SELECT
-                    fw.id,
-                    fw.title,
-                    fw.description,
-                    fw.rating,
-                    fw.type,
-                    fw.created,
-                    fw.modified,
-                    COALESCE (
-                        json_agg(
-                            DISTINCT jsonb_build_object(
-                                'id', p.id,
-                                'name', p.full_name
-                            )
-                        ) FILTER (WHERE p.id is not null and pfw.role = 'actor'),
-                        '[]'
-                    ) as actors,
-                    COALESCE (
-                        json_agg(
-                            DISTINCT jsonb_build_object(
-                                'id', p.id,
-                                'name', p.full_name
-                            )
-                        ) FILTER (WHERE p.id is not null and pfw.role = 'writer'),
-                        '[]'
-                    ) as writers,
-                    array_agg(DISTINCT g.name) as genre,
-                    array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'actor' ) as actors_names,
-                    array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'writer' ) as writers_names,
-                    array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'director' ) as director,
-                    GREATEST(fw.modified, MAX(p.modified), MAX(g.modified)) as greatest_modified
-                FROM content.film_work fw
-                LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
-                LEFT JOIN content.person p ON p.id = pfw.person_id
-                LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
-                LEFT JOIN content.genre g ON g.id = gfw.genre_id
-                WHERE fw.modified > '{last_modified}'
-                GROUP BY fw.id
-                ORDER BY fw.modified
-                LIMIT 500;
-        """)
-        data = cursor.fetchall()
-        try:
-            last_modified = data[-1][6]
-        except IndexError:
-            last_modified = datetime.min
-        self.redis_storage.set_key('last_modified', last_modified)
-        return data
+        with self.connection.cursor() as cursor:
+            cursor.execute(f"""
+                    SELECT
+                        fw.id,
+                        fw.title,
+                        fw.description,
+                        fw.rating,
+                        fw.type,
+                        fw.created,
+                        fw.modified,
+                        COALESCE (
+                            json_agg(
+                                DISTINCT jsonb_build_object(
+                                    'id', p.id,
+                                    'name', p.full_name
+                                )
+                            ) FILTER (WHERE p.id is not null and pfw.role = 'actor'),
+                            '[]'
+                        ) as actors,
+                        COALESCE (
+                            json_agg(
+                                DISTINCT jsonb_build_object(
+                                    'id', p.id,
+                                    'name', p.full_name
+                                )
+                            ) FILTER (WHERE p.id is not null and pfw.role = 'writer'),
+                            '[]'
+                        ) as writers,
+                        array_agg(DISTINCT g.name) as genre,
+                        array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'actor' ) as actors_names,
+                        array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'writer' ) as writers_names,
+                        array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'director' ) as director,
+                        GREATEST(fw.modified, MAX(p.modified), MAX(g.modified)) as greatest_modified
+                    FROM content.film_work fw
+                    LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+                    LEFT JOIN content.person p ON p.id = pfw.person_id
+                    LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+                    LEFT JOIN content.genre g ON g.id = gfw.genre_id
+                    WHERE fw.modified > '{last_modified}'
+                    GROUP BY fw.id
+                    ORDER BY fw.modified
+                    LIMIT 500;
+            """)
+            data = cursor.fetchall()
+            try:
+                last_modified = data[-1][6]
+            except IndexError:
+                last_modified = datetime.min
+            self.redis_storage.set_key('last_modified', last_modified)
+            return data
