a28180e0fa440c433554bb2475df12b89bf1e7bb
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2022-12-30 20:07:31 +0300

    added greatest_modified to query

--- etl/postgres_extraxtor.py
+++ etl/postgres_extraxtor.py
@@ -31,9 +31,8 @@ class PostgresExtractor:
                     COALESCE (
                         json_agg(
                             DISTINCT jsonb_build_object(
-                                'person_role', pfw.role,
-                                'person_id', p.id,
-                                'person_name', p.full_name
+                                'id', p.id,
+                                'name', p.full_name
                             )
                         ) FILTER (WHERE p.id is not null and pfw.role = 'actor'),
                         '[]'
@@ -41,9 +40,8 @@ class PostgresExtractor:
                     COALESCE (
                         json_agg(
                             DISTINCT jsonb_build_object(
-                                'person_role', pfw.role,
-                                'person_id', p.id,
-                                'person_name', p.full_name
+                                'id', p.id,
+                                'name', p.full_name
                             )
                         ) FILTER (WHERE p.id is not null and pfw.role = 'writer'),
                         '[]'
@@ -51,16 +49,16 @@ class PostgresExtractor:
                     COALESCE (
                         json_agg(
                             DISTINCT jsonb_build_object(
-                                'person_role', pfw.role,
-                                'person_id', p.id,
-                                'person_name', p.full_name
+                                'id', p.id,
+                                'name', p.full_name
                             )
                         ) FILTER (WHERE p.id is not null and pfw.role = 'director'),
                         '[]'
                     ) as director,
                     array_agg(DISTINCT g.name) as genres,
                     array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'actor' ) as actors_names,
-                    array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'writer' ) as writers_names
+                    array_agg(DISTINCT p.full_name) FILTER ( WHERE pfw.role = 'writer' ) as writers_names,
+                    GREATEST(fw.modified, MAX(p.modified), MAX(g.modified)) AS greatest_modified
                 FROM content.film_work fw
                 LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
                 LEFT JOIN content.person p ON p.id = pfw.person_id
