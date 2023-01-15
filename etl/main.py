d47f66aa1143f3d6cbb18840076ad4d630af2b25
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-01-15 19:41:42 +0300

    fixed etl_process so it can work with different models

--- etl/main.py
+++ etl/main.py
@@ -3,24 +3,27 @@ from time import sleep
 
 from elastic_loader import ElasticLoader
 from postgres_extractor import PostgresExtractor
-from settings import postgres_dsl, redis_config, elastic_config, LOGGER_SETTINGS
+from settings import postgres_dsl, redis_config, elastic_config, LOGGER_SETTINGS, INDEXES
 from state import RedisState
 
 
 def etl_process() -> None:
     """
     Создаются экземпляры RedisState, PostgresExtractor, ElasticLoader
-    В бесконечном цикле обходятся все фильмы, и отправляются в elastic_loader
+    В бесконечном цикле обходятся все записи из постгрес, и отправляются в elastic_loader
     """
     redis_storage = RedisState(redis_config)
     postgres_extractor = PostgresExtractor(postgres_dsl, redis_storage)
     elastic_loader = ElasticLoader(elastic_config)
     while True:
-        data = postgres_extractor.extract_data_from_db()
-        if data:
-            elastic_loader.update_elasticsearch(data)
-            continue
-        sleep(60.0)
+        for index_name, index_params in INDEXES.items():
+            model = index_params[1]
+            query = index_params[0]
+            data = postgres_extractor.extract_data_from_db(index_name=index_name, query_function=query)
+            if data:
+                elastic_loader.update_elasticsearch(data, index_model=model, index_name=index_name)
+                continue
+            sleep(15.0)
 
 
 if __name__ == '__main__':
