ccf14035a7f67a49171c6dd4e1751b7dee73518b
Александр Карулин <av.karulin@tensor.ru>
2023-01-16 21:22:11 +0300

    change person model

--- etl/models.py
+++ etl/models.py
@@ -8,16 +8,21 @@ class AbstractModel(BaseModel):
     id: UUID
 
 
-class Person(AbstractModel):
-    name: str
+class PersonShort(AbstractModel):
+    full_name: str
+
+
+class Person(PersonShort):
+    roles: Optional[List[str]]
+    film_ids: Optional[List[str]]
 
 
 class FilmWork(AbstractModel):
     title: str
     description: Optional[str] = None
     imdb_rating: float = Field(alias='rating')
-    actors: Optional[List[Person]] = None
-    writers: Optional[List[Person]] = None
+    actors: Optional[List[PersonShort]] = None
+    writers: Optional[List[PersonShort]] = None
     director: Optional[List] = None
     genre: Optional[List] = None
     actors_names: Optional[List] = None
